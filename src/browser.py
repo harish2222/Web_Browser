4c9747de93699e918a7ebd2eeb81294c1a49d249
Harish <harishkalva4126@gmail.com>
2021-03-06 18:07:09 +0530

    final for first release

--- src/browser.py
+++ src/browser.py
@@ -23,7 +23,30 @@ class MainWindow(QMainWindow):
         forward_btn.triggered.connect(self.browser.forward)
         navbar.addAction(forward_btn)
 
-        reload_btn = QAction
+        reload_btn = QAction("Reload",self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        home_btn = QAction('Home',self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        self.browser.urlChanged.connect(self.url_update)
+
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+    def url_update(self,g):
+        self.url_bar.setText(g.toString())
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl("https://google.com"))  
+
 
 App = QApplication(sys.argv)
 QApplication.setApplicationName("TOR Borwser")
